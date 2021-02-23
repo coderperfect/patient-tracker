ef67ce8c71eb05eab8653a53fcff14602f892557
Mrityunjoy Nath <mrityunjoynath10@gmail.com>
2021-02-23 23:30:49 +0530

    Prescription and Medicine Classes

--- src/main/java/com/tracker/patienttracker/model/Prescription.java
+++ src/main/java/com/tracker/patienttracker/model/Prescription.java
@@ -1,5 +1,24 @@
 package com.tracker.patienttracker.model;
 
-public class Prescription {
+import org.springframework.stereotype.Repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Repository
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Prescription {
+	private int prescriptionId;
+	private int medicineId;
+	private int quantity;
+	private double prescriptionCost;
+	private boolean paid;
+	private boolean billed;
 }
--- src/test/java/com/tracker/patienttracker/model/PrescriptionTest.java
+++ src/test/java/com/tracker/patienttracker/model/PrescriptionTest.java
@@ -1,5 +1,120 @@
 package com.tracker.patienttracker.model;
 
-public class PrescriptionTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class PrescriptionTest {
+	@Autowired
+	Prescription prescription;
+	
+	@Test
+	@Order(1)
+	public void testGetPrescriptionId(){
+		prescription.setPrescriptionId(1);
+		
+		assertEquals(1, prescription.getPrescriptionId());
+	}
+	
+	@Test
+	@Order(2)
+	public void testSetPrescriptionId(){
+		prescription.setPrescriptionId(1);
+		
+		assertEquals(1, prescription.getPrescriptionId());
+	}
+	
+	@Test
+	@Order(3)
+	public void testGetMedicinenId(){
+		prescription.setMedicineId(1);
+		
+		assertEquals(1, prescription.getMedicineId());
+	}
+	
+	@Test
+	@Order(4)
+	public void testSetMedicineId(){
+		prescription.setMedicineId(1);
+		
+		assertEquals(1, prescription.getMedicineId());
+	}
+	
+	@Test
+	@Order(5)
+	public void testGetQuantity(){
+		prescription.setQuantity(1);
+		
+		assertEquals(1, prescription.getQuantity());
+	}
+	
+	@Test
+	@Order(6)
+	public void testSetQuantity(){
+		prescription.setQuantity(1);
+		
+		assertEquals(1, prescription.getQuantity());
+	}
+	
+	@Test
+	@Order(7)
+	public void testGetPrescriptionCost(){
+		prescription.setPrescriptionCost(1);
+		
+		assertEquals(1, prescription.getPrescriptionCost());
+	}
+	
+	@Test
+	@Order(8)
+	public void testSetPrescriptionCost(){
+		prescription.setPrescriptionCost(1);
+		
+		assertEquals(1, prescription.getPrescriptionCost());
+	}
+	
+	@Test
+	@Order(9)
+	public void testIsPaid(){
+		prescription.setPaid(true);
+		
+		assertEquals(true, prescription.isPaid());
+	}
+	
+	@Test
+	@Order(10)
+	public void testSetPaid(){
+		prescription.setPaid(true);
+		
+		assertEquals(true, prescription.isPaid());
+	}
+	
+	@Test
+	@Order(11)
+	public void testIsBilled(){
+		prescription.setBilled(true);
+		
+		assertEquals(true, prescription.isBilled());
+	}
+	
+	@Test
+	@Order(12)
+	public void testSetBilled(){
+		prescription.setBilled(true);
+		
+		assertEquals(true, prescription.isBilled());
+	}
+	
+	@Test
+	@Order(13)
+	public void testToString(){
+		String expectedString = "Prescription(prescriptionId=1, medicineId=1, quantity=1, prescriptionCost=1.0, paid=true, billed=true)";
+		assertEquals(expectedString, prescription.toString());
+	}
 }
