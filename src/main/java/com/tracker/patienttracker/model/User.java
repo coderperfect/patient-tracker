c0f2a372dd6d9dc10fc6bc7bf3371c8babb12fcc
chandu7077 <chanduburre7@gmail.com>
2021-02-24 14:04:16 +0530

    Inheritence added

--- src/main/java/com/tracker/patienttracker/model/User.java
+++ src/main/java/com/tracker/patienttracker/model/User.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
