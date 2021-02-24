8fff894d981c49dc3ff55f2c497c612773fdbdb4
Mrityunjoy Nath <mrityunjoynath10@gmail.com>
2021-02-24 12:31:11 +0530

    Repository annot

--- src/main/java/com/tracker/patienttracker/model/Prescription.java
+++ src/main/java/com/tracker/patienttracker/model/Prescription.java
@@ -5,12 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.stereotype.Repository;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Repository
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
