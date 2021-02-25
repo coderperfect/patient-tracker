2a05e1a399ed4cd6686e2af11cb2c74cef50999c
chandu7077 <chanduburre7@gmail.com>
2021-02-25 10:30:19 +0530

    validations tested user/patient

--- src/main/java/com/tracker/patienttracker/model/Doctor.java
+++ src/main/java/com/tracker/patienttracker/model/Doctor.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,5 +25,4 @@ public class Doctor{
 	private String qualification;
 	private String specialization;
 	private double consultationFee;
-	private int userId;
 }
--- src/main/java/com/tracker/patienttracker/model/Patient.java
+++ src/main/java/com/tracker/patienttracker/model/Patient.java
@@ -2,6 +2,9 @@ package com.tracker.patienttracker.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.Pattern;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +17,11 @@ import lombok.NoArgsConstructor;
 public class Patient{
 	@Id
 	int patientId;
+	
+	@Pattern(regexp="^(O|A|B|AB)(+ve|-ve)$")
 	String bloodGroup;
 	
-	private int userId;
+	@OneToOne
+	@JoinColumn(name="userId")
+	private User user;
 }
