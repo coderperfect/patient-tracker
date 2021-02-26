19ed23d6f426da1dcef7a2ca5b709be182a8dfd2
coderperfect <mrityunjoynath38@gmail.com>
2021-02-26 09:40:43 +0530

    Repository and Tests

--- src/main/java/com/tracker/patienttracker/repository/InPatientRecordRepository.java
+++ src/main/java/com/tracker/patienttracker/repository/InPatientRecordRepository.java
@@ -8,13 +8,5 @@ import com.tracker.patienttracker.model.InPatientRecord;
 
 @Repository
 public interface InPatientRecordRepository extends JpaRepository<InPatientRecord, Integer>{
-	// List of In Patients
-		//Select * from inpatientrecord where dischargeDate = null
-		@Query(value = "Select i From InPatientRecord i where i.dischargeDate = null")
-		public InPatientRecord getAllInPatientRecords();
-		
-		// List of In Patients
-		//Select * from inpatientrecord where patientId=patientId
-		@Query(value = "Select i From InPatientRecord i left join i.patient p where p.patientId=:patientId")
-		public InPatientRecord getInPatientRecordByPatientId(int patientId);
+	
 }
